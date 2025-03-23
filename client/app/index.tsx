d0a2b7a75a309420f6cfd6db7e18adad18989f05
Ishrak-Adit07 <ishrak.adit07@gmail.com>
2025-03-23 16:47:18 +0600

    Configuration commit: Color codes

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -3,7 +3,12 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-5xl text-red-400">Welcome</Text>
+      <Text className="text-5xl text-primary font-semibold">Welcome</Text>
+      <Text className="text-3xl text-accent font-thin mt-4">
+        to my movie app
+      </Text>
+
+      {/* <Text className="text-xl text-dark-100 font-thin mt-4">example text</Text> */}
     </View>
   );
 }
