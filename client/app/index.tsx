da56ab39d6e47cda57a294ed0444c09a871732ba
Ishrak-Adit07 <ishrak.adit07@gmail.com>
2025-03-23 16:57:12 +0600

    Routing config: Static and Dynamic

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
@@ -7,6 +8,17 @@ export default function Index() {
       <Text className="text-3xl text-accent font-thin mt-4">
         to my movie app
       </Text>
+
+      <Link href="/onboarding" className="mt-4">
+        Onboarding
+      </Link>
+
+      <Link
+        href={{ pathname: "/movie/[id]", params: { id: "Avengers" } }}
+        className="mt-4"
+      >
+        Avengers
+      </Link>
 
       {/* <Text className="text-xl text-dark-100 font-thin mt-4">example text</Text> */}
     </View>
