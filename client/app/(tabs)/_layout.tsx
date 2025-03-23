3c41b430e90f95b6b289813033ff1134d12a4632
Ishrak-Adit07 <ishrak.adit07@gmail.com>
2025-03-23 20:08:15 +0600

    Design commit: Attributes of 'Tabs'

--- client/app/(tabs)/_layout.tsx
+++ client/app/(tabs)/_layout.tsx
@@ -9,10 +9,10 @@ const TabIcon = ({ focused, icon, title }: any) => {
     return (
       <ImageBackground
         source={images.highlight}
-        className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"
+        className="flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden"
         resizeMode="cover"
       >
-        <Image source={icon} className="size-5" />
+        <Image source={icon} className="size-5" tintColor={"#151312"} />
         <Text className="text-secondary text-base font-semibold ml-2">
           {title}
         </Text>
@@ -28,7 +28,28 @@ const TabIcon = ({ focused, icon, title }: any) => {
 
 const _layout = () => {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarItemStyle: {
+          width: "100%",
+          height: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        },
+        tabBarStyle: {
+          backgroundColor: "#0F0D23",
+          borderRadius: 50,
+          marginHorizontal: 20,
+          marginBottom: 36,
+          height: 52,
+          position: "absolute",
+          overflow: "hidden",
+          borderWidth: 1,
+          borderColor: "#0F0D23",
+        },
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
