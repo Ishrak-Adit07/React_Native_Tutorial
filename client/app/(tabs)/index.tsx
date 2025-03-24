f1e7933c5833dd2d541279bfd27776dcf77dee38
Ishrak-Adit07 <ishrak.adit07@gmail.com>
2025-03-25 02:39:43 +0600

    Custom hook use commit: Fetch movies in Index page: Unsuccessful

--- client/app/(tabs)/index.tsx
+++ client/app/(tabs)/index.tsx
@@ -1,11 +1,24 @@
-import { Image, ScrollView } from "react-native";
-import { Link, router } from "expo-router";
+import { ActivityIndicator, Image, ScrollView } from "react-native";
+import { Link, router, useRouter } from "expo-router";
 import { Text, View } from "react-native";
 import { images } from "@/constants/images";
 import { icons } from "@/constants/icons";
 import SearchBar from "@/components/SearchBar";
+import useFetch from "@/services/useFetch";
+import { fetchMovies } from "@/services/api";
 
 export default function Index() {
+  const router = useRouter();
+  const {
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError,
+  } = useFetch(() =>
+    fetchMovies({
+      query: "",
+    })
+  );
+
   return (
     <View className="flex-1 bg-primary">
       <Image source={images.bg} className="absolute w-full z-0" />
@@ -19,14 +32,32 @@ export default function Index() {
       >
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
 
-        <View className="flex-1 mt-5">
-          <SearchBar
-            placeHolder="Search for a movie"
-            onPress={() => {
-              router.push("/search");
-            }}
+        {moviesLoading ? (
+          <ActivityIndicator
+            size={"large"}
+            color={"#0000ff"}
+            className="mt-10 self-center"
           />
-        </View>
+        ) : moviesError ? (
+          <Text className="text-lg text-white font-bold mt-5 mb-3">
+            Error: {moviesError?.message}
+          </Text>
+        ) : (
+          <View className="flex-1 mt-5">
+            <SearchBar
+              placeHolder="Search for a movie"
+              onPress={() => {
+                router.push("/search");
+              }}
+            />
+
+            <>
+              <Text className="text-lg text-white font-bold mt-5 mb-3">
+                Latest Movies
+              </Text>
+            </>
+          </View>
+        )}
       </ScrollView>
     </View>
   );
