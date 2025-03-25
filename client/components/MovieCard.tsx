3d390e39902cbbfec02e584e94711b94caa6d467
Ishrak-Adit07 <ishrak.adit07@gmail.com>
2025-03-25 19:45:46 +0600

    Movie card display commit

--- client/components/MovieCard.tsx
+++ client/components/MovieCard.tsx
@@ -1,6 +1,7 @@
 import { View, Text, TouchableOpacity, Image } from "react-native";
 import React from "react";
 import { Link } from "expo-router";
+import { icons } from "@/constants/icons";
 
 const MovieCard = ({
   id,
@@ -22,7 +23,25 @@ const MovieCard = ({
           resizeMode="cover"
         />
 
-        <Text className="text-white text-sm font-bold mt-2">{title}</Text>
+        <Text className="text-white text-sm font-bold mt-2" numberOfLines={1}>
+          {title}
+        </Text>
+
+        <View className="flex-row items-center justify-start gap-x-1">
+          <Image source={icons.star} className="size-4" />
+          <Text className="text-sx text-white font-bold uppercase">
+            {Math.round(vote_average / 2)}
+          </Text>
+        </View>
+
+        <View className="flex-row items-center justify-between">
+          <Text className="text-xs text-light-300 font-medium mt-1">
+            {release_date?.split("-")[0]}
+          </Text>
+          {/* <Text className="text-xs text-light-300 font-medium uppercase">
+            Movie
+          </Text> */}
+        </View>
       </TouchableOpacity>
     </Link>
   );
